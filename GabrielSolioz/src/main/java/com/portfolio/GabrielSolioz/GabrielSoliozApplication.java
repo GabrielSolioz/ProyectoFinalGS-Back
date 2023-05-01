d76c7799f78682bcb87801b657ac99c9d8afdfbb
Gabriel Solioz <gab.solioz111@gmail.com>
2023-05-01 15:19:46 -0300

    creo el proyecto con spring

---
+++ GabrielSolioz/src/main/java/com/portfolio/GabrielSolioz/GabrielSoliozApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.GabrielSolioz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielSoliozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielSoliozApplication.class, args);
+	}
+
+}
---
+++ GabrielSolioz/src/test/java/com/portfolio/GabrielSolioz/GabrielSoliozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.GabrielSolioz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrielSoliozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
